f8d67b055ec8105e2d476444b6edd6770f4ca807
Sch <junhg0211@gmail.com>
2023-06-01 10:18:31 +0900

    simplify ClientThread run()

--- src/org/shtelo/sch/vending_project/server/ClientThread.java
+++ src/org/shtelo/sch/vending_project/server/ClientThread.java
@@ -53,127 +53,20 @@ public class ClientThread extends Thread {
             String command = message.substring(0, 4);
 
             if (command.equalsIgnoreCase("AUTH")) {
-                if (message.length() < 5) {
-                    sendMessage("5 잘못된 명령어");
-                    continue;
-                }
-
-                String password = message.substring(5);
-                String errorMessage = Util.confirmLogin(password);
-
-                if (!errorMessage.isEmpty()) {
-                    sendMessage("1 인증 실패: " + errorMessage);
-                    continue;
-                }
-
-                if (server.getAuthorizeds().contains(address)) {
-                    sendMessage("1 인증 실패: 이미 인증됨");
-                    continue;
-                }
-
-                sendMessage("0 OK");
-                server.getAuthorizeds().add(address);
-            }
-            else if (command.equalsIgnoreCase("GSEL")) {
-                if (!server.getAuthorizeds().contains(address)) {
-                    sendMessage("1 인증 실패");
-                    continue;
-                }
-
-                if (message.length() < 5) {
-                    sendMessage("5 잘못된 명령어");
-                    continue;
-                }
-
-                SellLogger logger = machine.getSellLogger();
-                String date = message.substring(5);
-                if (!logger.isLogOn(date)) {
-                    sendMessage("1 로그 없음");
-                    continue;
-                }
-
-                DailyLog log = logger.getLog(date);
-                HashMap<String, Integer> sells = log.getSells();
-                sendMessage("2 " + sells.size());
-                sells.forEach((kind, amount) -> sendMessage(String.format("%s %d", kind, amount)));
-            }
-            else if (command.equalsIgnoreCase("GINV")) {
-                if (!server.getAuthorizeds().contains(address)) {
-                    sendMessage("1 인증 실패");
-                    continue;
-                }
-
-                sendMessage("3 5");
-                List<Product> juices = machine.getInventory().getJuices();
-                for (Product juice : juices) {
-                    Kind kind = juice.getKind();
-                    sendMessage(String.format("%s %d %d", kind.getName(), kind.getPrice(), juice.getAmount()));
-                }
-            }
-            else if (command.equalsIgnoreCase("GLOG")) {
-                if (!server.getAuthorizeds().contains(address)) {
-                    sendMessage("1 인증 실패");
-                    continue;
-                }
-
-                if (message.length() < 6) {
-                    sendMessage("5 잘못된 명령어");
-                    continue;
-                }
-
-                int preferredCount = Integer.parseInt(message.substring(5));
-                LogFetcher fetcher = new LogFetcher();
-                ArrayList<String> logs = new ArrayList<>();
-                for (int i = 0; i < preferredCount && !fetcher.isEnd(); i++) {
-                    logs.add(fetcher.getCurrent());
-                    fetcher.updateLast();
-                }
-
-                sendMessage("2 " + logs.size());
-                for (String log : logs) {
-                    sendMessage(log);
-                }
-            }
-            else if (command.equalsIgnoreCase("SPRD")) {
-                if (!server.getAuthorizeds().contains(address)) {
-                    sendMessage("1 인증 실패");
-                    continue;
-                }
-
-                if (message.length() < 5) {
-                    sendMessage("5 잘못된 명령어");
-                    continue;
-                }
-
-                String[] tokens = message.substring(5).split(" ", 3);
-                if (tokens.length < 3) {
-                    sendMessage("5 잘못된 명령어");
-                    continue;
-                }
-
-                int index = Integer.parseInt(tokens[0]);
-                int amount = Integer.parseInt(tokens[1]);
-                String name = tokens[2];
-
-                machine.updateProductName(index, name);
-                machine.updateLeftProductAmount(index, amount);
-
-                sendMessage("0 OK");
-            }
-            else if (command.equalsIgnoreCase("QUIT")) {
-                connected = false;
-                sendMessage("4 통신 종료");
-            }
-            else if (command.equalsIgnoreCase("IATH")) {
-                boolean contained = server.getAuthorizeds().contains(address);
-
-                if (contained) {
-                    sendMessage("0 OK");
-                } else {
-                    sendMessage("1 인증 실패");
-                }
-            }
-            else {
+                handleAuth(message, address);
+            } else if (command.equalsIgnoreCase("GSEL")) {
+                handleGsel(address, message);
+            } else if (command.equalsIgnoreCase("GINV")) {
+                handleGinv(address);
+            } else if (command.equalsIgnoreCase("GLOG")) {
+                handleGlog(address, message);
+            } else if (command.equalsIgnoreCase("SPRD")) {
+                handleSprd(address, message);
+            } else if (command.equalsIgnoreCase("IATH")) {
+                handleIath(address);
+            } else if (command.equalsIgnoreCase("QUIT")) {
+                handleQuit();
+            } else {
                 sendMessage("5 잘못된 명령어");
             }
         }
@@ -189,6 +82,134 @@ public class ClientThread extends Thread {
         }
 
         server.getAuthorizeds().remove(String.format("%s:%d", socket.getInetAddress(), socket.getPort()));
+    }
+
+    private void handleQuit() {
+        connected = false;
+        sendMessage("4 통신 종료");
+    }
+
+    private void handleGsel(String address, String message) {
+        if (!server.getAuthorizeds().contains(address)) {
+            sendMessage("1 인증 실패");
+            return;
+        }
+
+        if (message.length() < 5) {
+            sendMessage("5 잘못된 명령어");
+            return;
+        }
+
+        SellLogger logger = machine.getSellLogger();
+        String date = message.substring(5);
+        if (!logger.isLogOn(date)) {
+            sendMessage("1 로그 없음");
+            return;
+        }
+
+        DailyLog log = logger.getLog(date);
+        HashMap<String, Integer> sells = log.getSells();
+        sendMessage("2 " + sells.size());
+        sells.forEach((kind, amount) -> sendMessage(String.format("%s %d", kind, amount)));
+    }
+
+    private void handleGinv(String address) {
+        if (!server.getAuthorizeds().contains(address)) {
+            sendMessage("1 인증 실패");
+            return;
+        }
+
+        sendMessage("3 5");
+        List<Product> juices = machine.getInventory().getJuices();
+        for (Product juice : juices) {
+            Kind kind = juice.getKind();
+            sendMessage(String.format("%s %d %d", kind.getName(), kind.getPrice(), juice.getAmount()));
+        }
+    }
+
+    private void handleGlog(String address, String message) {
+        if (!server.getAuthorizeds().contains(address)) {
+            sendMessage("1 인증 실패");
+            return;
+        }
+
+        if (message.length() < 6) {
+            sendMessage("5 잘못된 명령어");
+            return;
+        }
+
+        int preferredCount = Integer.parseInt(message.substring(5));
+        LogFetcher fetcher = new LogFetcher();
+        ArrayList<String> logs = new ArrayList<>();
+        for (int i = 0; i < preferredCount && !fetcher.isEnd(); i++) {
+            logs.add(fetcher.getCurrent());
+            fetcher.updateLast();
+        }
+
+        sendMessage("2 " + logs.size());
+        for (String log : logs) {
+            sendMessage(log);
+        }
+    }
+
+    private void handleSprd(String address, String message) {
+        if (!server.getAuthorizeds().contains(address)) {
+            sendMessage("1 인증 실패");
+            return;
+        }
+
+        if (message.length() < 5) {
+            sendMessage("5 잘못된 명령어");
+            return;
+        }
+
+        String[] tokens = message.substring(5).split(" ", 3);
+        if (tokens.length < 3) {
+            sendMessage("5 잘못된 명령어");
+            return;
+        }
+
+        int index = Integer.parseInt(tokens[0]);
+        int amount = Integer.parseInt(tokens[1]);
+        String name = tokens[2];
+
+        machine.updateProductName(index, name);
+        machine.updateLeftProductAmount(index, amount);
+
+        sendMessage("0 OK");
+    }
+
+    private void handleIath(String address) {
+        boolean contained = server.getAuthorizeds().contains(address);
+
+        if (contained) {
+            sendMessage("0 OK");
+        } else {
+            sendMessage("1 인증 실패");
+        }
+    }
+
+    private void handleAuth(String message, String address) {
+        if (message.length() < 5) {
+            sendMessage("5 잘못된 명령어");
+            return;
+        }
+
+        String password = message.substring(5);
+        String errorMessage = Util.confirmLogin(password);
+
+        if (!errorMessage.isEmpty()) {
+            sendMessage("1 인증 실패: " + errorMessage);
+            return;
+        }
+
+        if (server.getAuthorizeds().contains(address)) {
+            sendMessage("1 인증 실패: 이미 인증됨");
+            return;
+        }
+
+        sendMessage("0 OK");
+        server.getAuthorizeds().add(address);
     }
 
     public void sendMessage(String message) {
